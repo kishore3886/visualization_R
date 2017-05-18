170603c813b2d0487fb538b8be7cc360ac308a36
kishore <kishore3886@gmail.com>
2017-05-17 20:10:19 -0400

    started R with Git

---
+++ Basic_plotting.R
@@ -0,0 +1,22 @@
+##-------------- Basic Plotting in R --------------
+
+# See structure of the iris data frame 
+
+
+
+
+# Plot Sepal length and width on x and y axes respectively
+
+
+
+
+# Add a layer of petal length and width using points, use col = "red" 
+
+
+
+
+# Plot Species (factor) as colors; note that now we assign color to a variable in the data frame 
+# and R assigns a color to each species automatically
+
+
+
