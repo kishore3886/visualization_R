e12b3e6e178eb4e245ebcff89f5c15bb76b53861
kishore <kishore3886@gmail.com>
2017-05-17 21:28:00 -0400

    ggplot practice

---
+++ creating_visualizations_using_ggplot.R
@@ -0,0 +1,38 @@
+#----------Creating Visualizations Using ggplot()------------
+
+
+# --------Plotting using ggplot2: mtcars data frame----------
+
+# mtcars has observations of 32 cars with miles per gallon, cylinders, weight, no of gears etc.
+
+
+# Check structure of mtcars 
+
+
+
+# This is the first plot using ggplot2 using the built-in data frame mtcars.
+# Plot cyl on x and mpg on y axis
+
+
+# Identify which elements are in data, aes() and geom layers 
+# Note that there are no 5 or 7 cylinder cars, but the plot shows 5 and 7 on x axis. Why?
+
+
+
+
+# Use factor(cyl) this time, don't change anything else in the plot above
+# ggplot can convert a num variable to a factor variable. Looks better, right?
+
+
+
+# Reverse the order of variables: plot mpg on x and factor(cyl) on y 
+
+
+
+# Plot am versus mpg (on x and y axes respectively)
+
+
+
+# Should you use factor() here? 
+
+
