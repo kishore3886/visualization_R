c19bb7c5ad76da05241ff28f4391c4583cfd9352
kishore <kishore3886@gmail.com>
2017-05-17 20:26:52 -0400

    after plotting iris

--- Basic_plotting.R
+++ Basic_plotting.R
@@ -1,22 +1,31 @@
 ##-------------- Basic Plotting in R --------------
+iris<-iris
 
 # See structure of the iris data frame 
 
 
-
+View(iris)
+str(iris)
 
 # Plot Sepal length and width on x and y axes respectively
 
 
-
+plot(iris$Sepal.Length,iris$Sepal.Width)
 
 # Add a layer of petal length and width using points, use col = "red" 
 
-
+points(iris$Petal.Length,iris$Petal.Width,col="red")
 
 
 # Plot Species (factor) as colors; note that now we assign color to a variable in the data frame 
 # and R assigns a color to each species automatically
 
 
+plot(iris$Sepal.Length,iris$Sepal.Width,col=iris$Species)
 
+
+par(mfrow = c(1,2))
+
+plot(iris$Petal.Length, iris$Petal.Width, col = iris$Species, main = "iris", xlab = "Petal Length", ylab = "Petal Width")
+
+plot(iris$Sepal.Length, iris$Sepal.Width, col = iris$Species, main = "iris", xlab = "Sepal Length", ylab = "Sepal Width")
